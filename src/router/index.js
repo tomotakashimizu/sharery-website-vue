1bf7f849452e49d9153f8d8ac477c66de347a5da
tomotakashimizu <tomotaka.developer@gmail.com>
2021-06-23 20:35:33 +0900

    TermsOfService, PrivacyPolicy の path を設定

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from "vue"
 import VueRouter from "vue-router"
 import Home from "../views/Home.vue"
 import About from "../views/About.vue"
+import TermsOfService from "../views/TermsOfService"
+import PrivacyPolicy from "../views/PrivacyPolicy"
 
 Vue.use(VueRouter)
 
@@ -15,6 +17,16 @@ const routes = [
     path: "/about",
     name: "About",
     component: About,
+  },
+  {
+    path: "/terms-of-service",
+    name: "TermsOfService",
+    component: TermsOfService,
+  },
+  {
+    path: "privacy-policy",
+    name: "PrivacyPolicy",
+    component: PrivacyPolicy,
   },
 ]
 
