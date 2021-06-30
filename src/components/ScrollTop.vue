d76e2fb63f717c5a31830992c046af8bc5eab913
tomotakashimizu <tomotaka.developer@gmail.com>
2021-06-30 21:47:35 +0900

    [update] スクロールによってフェードイン・アウトするボタンを作成
    参照記事
    https://zenn.dev/han_aru/articles/d0ffed8e1db18d

--- src/components/ScrollTop.vue
+++ src/components/ScrollTop.vue
@@ -1,17 +1,36 @@
 <template>
   <div>
-    <div class="page-button" v-on:click="scrollTopSmooth">
-      <i class="fas fa-chevron-up page-button-icon"></i>
-    </div>
+    <transition>
+      <div v-show="visible">
+        <div class="page-button" v-on:click="scrollTopSmooth">
+          <i class="fas fa-chevron-up page-button-icon"></i>
+        </div>
+        <h1 id="header-logo">ここがフェードイン・アウトするよ</h1>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      visible: false,
+    }
+  },
   created() {
+    window.addEventListener("scroll", this.handleScroll)
     this.scrollTopInstant()
   },
   methods: {
+    handleScroll() {
+      this.scrollY = window.scrollY
+      if (!this.visible) {
+        this.visible = window.scrollY > 100
+      } else if (window.scrollY < 90) {
+        this.visible = !this.visible
+      }
+    },
     scrollTopSmooth() {
       window.scrollTo({
         top: 0,
@@ -29,6 +48,32 @@ export default {
 </script>
 
 <style scoped>
+#header-logo {
+  position: fixed;
+  top: 5%;
+  left: 9%;
+}
+
+/* ヘッダーロゴ フェード処理ここから */
+.v-enter {
+  opacity: 0;
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 1s;
+}
+
+.v-enter-to {
+  opacity: 1;
+}
+
+.v-leave-to {
+  opacity: 0;
+  transition: opacity 1s;
+}
+/* ヘッダーロゴ フェード処理ここまで */
+
 .page-button {
   position: fixed;
   right: 14px;
