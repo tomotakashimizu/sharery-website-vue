f3114c3666913a401183509dd7e0408ef711aaef
tomotakashimizu <tomotaka.developer@gmail.com>
2021-07-07 00:06:21 +0900

    [add] 画像を表示するコンポーネントを作成_途中
    説明文付きの画像表示を効率的に行えるコンポーネントを考える必要性がある

---
+++ src/components/Preview.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <img
+      alt="Vue logo"
+      src="../assets/previews/iPhone12ProMax_01.jpeg"
+      class="preview-image"
+    />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.preview-image {
+  /* width: 1000px; */
+  max-width: 100%;
+  height: auto;
+  background-size: cover;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <Preview />
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
     <ScrollTop />
@@ -10,12 +11,14 @@
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue"
 import ScrollTop from "@/components/ScrollTop.vue"
+import Preview from "@/components/Preview.vue"
 
 export default {
   name: "Home",
   components: {
     HelloWorld,
     ScrollTop,
+    Preview,
   },
 }
 </script>
